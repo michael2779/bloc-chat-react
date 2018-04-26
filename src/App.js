20c2e67eb36dbfcd5def6c52f617221fac5f14cb
Michael Yosowitz <michaelyosowitz@gmail.com>
2018-04-26 16:29:30 -0700

    firebase added

--- src/App.js
+++ src/App.js
@@ -1,6 +1,18 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import * as firebase from 'firebase';
+
+// Initialize Firebase
+var config = {
+  apiKey: "AIzaSyBdlHXk-lpxc8fxgZqfirPNDEDSNivN00o",
+  authDomain: "bloc-chat-react-4f261.firebaseapp.com",
+  databaseURL: "https://bloc-chat-react-4f261.firebaseio.com",
+  projectId: "bloc-chat-react-4f261",
+  storageBucket: "bloc-chat-react-4f261.appspot.com",
+  messagingSenderId: "658171361082"
+};
+firebase.initializeApp(config);
 
 class App extends Component {
   render() {
