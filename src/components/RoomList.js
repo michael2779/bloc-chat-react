9e248a9d20ac307d71fc88b6abe6c9c89b223576
Michael Yosowitz <michaelyosowitz@gmail.com>
2018-04-27 12:02:01 -0700

    new chat rooms

--- src/components/RoomList.js
+++ src/components/RoomList.js
@@ -1,15 +1,23 @@
 import React, { Component } from 'react';
 import '../App.css';
+import ReactModal from 'react-modal';
+
+ReactModal.setAppElement('#root');
 
 class RoomList extends Component {
     constructor(props){
         super(props);
 
         this.state = {
-            rooms: []
+            rooms: [],
+            showModal:false,
+            newRoomName: ''
         };
 
         this.roomsRef = this.props.firebase.database().ref('rooms');
+        this.handleOpenModal = this.handleOpenModal.bind(this);
+        this.handleCloseModal = this.handleCloseModal.bind(this);
+
     }
 
 
@@ -22,20 +30,57 @@ class RoomList extends Component {
         });
     }
 
+    handleOpenModal () {
+        this.setState({ showModal: true });
+    }
+      
+    handleCloseModal () {
+        this.setState({ showModal: false });
+    }
+
+    handleChange(event) {
+        this.setState({newRoomName: event.target.value});
+    }
+
+    createRoom(event) {
+        event.preventDefault();
+        
+        let newRoomPush = this.state.newRoomName;
+        
+        this.roomsRef.push({
+             name:newRoomPush
+        });
+        
+        this.setState({newRoomName:""});
+        
+        this.handleCloseModal();
+    }
+
     render(){
+
         return(
             <section className="section_left">
+                
                 {
-                    this.state.rooms.map( ( room ) =>
-                        
-                            <div key={room.key}>{room.name}</div>
-                        
+                    this.state.rooms.map( ( room ) =>    
+                        <div className='roomStyle' key={room.key}>{room.name}</div>
                     )
                 }
+                
+                <button onClick={this.handleOpenModal}>add new</button>
 
+                <ReactModal 
+                    isOpen={this.state.showModal}
+                    onRequestClose={this.handleCloseModal}
+                    className="Modal"
+                >
+                    <form onSubmit={this.createRoom.bind(this)}>              
+                        <input type="text" value={ this.state.newRoomName } onChange={ this.handleChange.bind(this) } />
+                        <button type="submit">Submit</button>
+                    </form>
+                </ReactModal>
 
             </section>
-            
         );
     }
     
